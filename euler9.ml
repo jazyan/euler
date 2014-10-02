f662af3f403b7f883e32bfdc75ddd9a60d8b5017
jasmineyan <jasmineyan@college.harvard.edu>
2014-10-02 12:15:50 -0400

    finished euler9

--- euler9.ml
+++ euler9.ml
@@ -1,2 +1,11 @@
-let rec is_pythag_tri (x : int) (y : int) (z : int) : bool =
-    
+let rec find_triple (i : int) (j : int) : int =
+    match j with
+    | 14 -> if i < 32 then (find_triple (i + 1) 0) else 0
+    | k -> if (k*k + i*i) + 2 * k * i + (i*i - k*k) = 1000 then ((k*k + i*i) * 2 * k * i * (i*i - k*k))
+            else find_triple i (k + 1)
+;;
+
+print_int(find_triple 0 0);;
+
+
+
